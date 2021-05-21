b079ef8152612dd3da8dfedb8851f6b2e873556f
Cameron Schuette <cam.r.schuette@gmail.com>
2021-05-21 11:39:58 -0400

    add path to child spawn

--- lib/deploy-command.ts
+++ lib/deploy-command.ts
@@ -23,28 +23,31 @@ export class DeployCommand {
      * @return void
      */
     public execute(): void {
-        const appStack = `--app "cdk.out/" "${this.stack}"`;
+        const appStack = `--app 'cdk.out/' ${this.stack}`;
 
-        let command: string = "";
+        const commands: string[] = [];
         if (this.type === DeploymentType.DEPLOY) {
-            command = `cdk deploy ${appStack}`;
+            commands.push(`deploy ${appStack}`);
         } else if (this.type === DeploymentType.DESTROY) {
-            command = `cdk destroy ${appStack}`;
+            commands.push(`destroy ${appStack}`);
         } else {
             throw new Error("Invalid enum value.");
         }
 
-        command += ` --output=./cdk_stacks/${this.stack}`;
-        command += ` --progress events --require-approval never`;
+        //commands.push(`--output=./cdk_stacks/${this.stack}`, `--progress events --require-approval never`)
 
-        cprint(PrintColors.FG_BLUE, `Executing command: ${command}.`);
+        cprint(PrintColors.FG_BLUE, `Executing command: cdk ${commands.join(' ')}.`);
 
-        const child = spawn(command, {
+        const child = spawn('cdk', commands, {
             cwd: this.path,
-            env: this.environment
+            env: {
+                PATH: process.env.PATH
+            }
         });
 
-        process.stdin.pipe(child.stdin)
+        process.stdin.pipe(child.stdin);
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
 
         child.stdout.on('data', (data) => {
             console.log(`child stdout:\n${data}`);
